fbd3ce7500a59baad1474432e7f89f34058aa9e6
DV-Park <98018435+DV-Park@users.noreply.github.com>
2022-08-02 15:33:53 +0900

    Initial commit

---
+++ Source/LUTIA/LUTIAGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LUTIAGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALUTIAGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALUTIAGameMode();
+};
+
+
+
