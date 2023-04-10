ec96420c3a0082e3f4f91b646d21bbfab7962984
DV-Park <98018435+DV-Park@users.noreply.github.com>
2023-04-10 19:06:57 +0900

    Skill 및 여러가지 만드는중

---
+++ Source/LUTIA/Public/LUTIA_SaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "LUTIA_SaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUTIA_API ULUTIA_SaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
